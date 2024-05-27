12ce395f7c1fedac86ebe3580bf2bc5013c51561
charmingruby <mrrubycanal3105@gmail.com>
2024-05-26 22:21:44 -0300

    feat: initial commit with supervisor cap

---
+++ .iex.exs
@@ -0,0 +1,8 @@
+emails = [
+  "dummy_1@email.com",
+  "dummy_2@email.com",
+  "dummy_3@email.com",
+  "dummy_4@email.com"
+]
+
+alias Sendex.Email.Sender
---
+++ lib/sendex/email/sender.ex
@@ -0,0 +1,24 @@
+defmodule Sendex.Email.Sender do
+  require Logger
+
+  @delay_in_seconds 1
+
+  def send_email("dummy_3@email.com" = email),
+    do: raise("Oops, couldn't send email to '#{email}'")
+
+  def send_email(email) do
+    delay = @delay_in_seconds * 1000
+    Process.sleep(delay)
+
+    Logger.info("Email to '#{email}' sent")
+
+    {:ok, "email_sent", email}
+  end
+
+  def notify_all(emails) do
+    Sendex.EmailTaskSupervisor
+    |> Task.Supervisor.async_stream_nolink(emails, &send_email/1)
+    |> IO.inspect()
+    |> Enum.to_list()
+  end
+end
